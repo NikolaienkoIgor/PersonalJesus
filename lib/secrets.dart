97da1092b9ed3ffae42365caa343a943a20e3938
NikolaienkoIgor <57862509+NikolaienkoIgor@users.noreply.github.com>
2023-06-29 20:40:43 +0200

    secret

--- lib/secrets.dart
+++ lib/secrets.dart
@@ -1,4 +1,4 @@
 // const openAiApiKey = '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';
-const openAiApiKey = 'sk-bLXv0PEz1IsQYPpkzlFiT3BlbkFJ00GurudmUc9fjGJMgUVtr';
+const openAiApiKey = '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';
 // const openAiOrg = 'YOUR_ORGANIZATION_ID';
 // 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
